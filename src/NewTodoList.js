cb5755c5a9c3648f5638c8de6affb503b0fb6a0b
lyliu <lyliu@thoughtworks.com>
2018-11-25 16:28:53 +0800

    [lyliu]: Implement delete todo item function.

--- src/NewTodoList.js
+++ src/NewTodoList.js
@@ -33,7 +33,9 @@ class NewTodoList extends Component{
             style={{width: '373px', marginTop: '10px'}}
             bordered
             dataSource={this.state.list}
-            renderItem={item => (<List.Item>{item}</List.Item>)}
+            renderItem={(item,index) => (
+              <List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>
+            )}
           />
         </div>
       </div>
@@ -58,6 +60,14 @@ class NewTodoList extends Component{
   handleBtnClick(){
     const action = {
       type:'add_todo_item',
+    }
+    store.dispatch(action);
+  }
+
+  handleItemDelete(index){
+    const action = {
+      type: 'delete_todo_item',
+      index    //是index: index的简写
     }
     store.dispatch(action);
   }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,7 @@
 //reducer类比为记录本
 const defaultState={
-  inputValue: '123',
-  list: [1,2]
+  inputValue: '',
+  list: []
 }
 
 //reducer可以接收state，但是不能修改state，所以需要修改时，一般使用深拷贝，然后修改拷贝出来的那份数据，最后返回
@@ -15,7 +15,11 @@ export default (state=defaultState, action) => {
     const newState = JSON.parse(JSON.stringify(state));//深拷贝
     newState.list.push(newState.inputValue);
     newState.inputValue = '';
-    console.log(newState);
+    return newState;
+  }
+  if(action.type === 'delete_todo_item') {
+    const newState = JSON.parse(JSON.stringify(state));//深拷贝
+    newState.list.splice(action.index,1);
     return newState;
   }
   return state;
